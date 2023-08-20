8f49b00e17e0919265a120264ae7e9d495798e22
MoeShair <hamoda.tam@gmail.com>
2023-08-20 10:08:29 +0300

    splitted the app, added shopping list module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
@@ -14,34 +12,35 @@ import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {RecipesModule} from "./recipes/reccipes.module";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
+    declarations: [
+        AppComponent,
+        HeaderComponent,
 
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
 
-    AuthComponent,
-    LoadingSpinnerComponent
-  ],
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    RecipesModule
-  ],
-  providers: [ShoppingListService, RecipeService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
-  bootstrap: [AppComponent]
+        DropdownDirective,
+
+        AuthComponent,
+        LoadingSpinnerComponent
+    ],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        RecipesModule,
+        ShoppingListModule
+    ],
+    providers: [ShoppingListService, RecipeService, {
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptorService,
+        multi: true
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
