e7b31a625b177251af3abc8bdde186da72bc522b
MoeShair <hamoda.tam@gmail.com>
2023-08-20 10:39:18 +0300

    splitted the app, added Auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,32 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeService} from "./recipes/recipe.serveice";
-import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {RecipesModule} from "./recipes/reccipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
+import {AuthModule} from "./auth/auth.module";
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
-        AuthComponent
+        HeaderComponent
     ],
     imports: [
-        FormsModule,
-        ReactiveFormsModule,
         BrowserModule,
         HttpClientModule,
         AppRoutingModule,
         RecipesModule,
+        HttpClientModule,
         ShoppingListModule,
-        SharedModule
+        SharedModule,
+        CoreModule,
+        AuthModule
     ],
-    providers: [ShoppingListService, RecipeService, {
-        provide: HTTP_INTERCEPTORS,
-        useClass: AuthInterceptorService,
-        multi: true
-    }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from "@angular/core";
+import {AuthComponent} from "./auth.component";
+import {SharedModule} from "../shared/shared.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+    declarations:[AuthComponent],
+    imports:[
+        SharedModule,
+        FormsModule,
+        RouterModule.forChild([
+            {path: 'auth', component:AuthComponent}
+        ])
+    ]
+})
+export class AuthModule{
+
+}
